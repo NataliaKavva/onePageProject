1cf5396da0d683b5d3d5cd81a8d05f8c2831788e
NataliaKavva <38726265+NataliaKavva@users.noreply.github.com>
2018-04-26 16:23:27 +0200

    The files have been added

---
+++ myFirstOnePageProject/scripts/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+  var $header=$('header');
+  var $sticky=$header.before($header.clone().addClass("sticky"));
+
+  $(window).on("scroll",function(){
+    var scrollFromTop=$(window).scrollTop();
+    $("body").toggleClass("scroll",(scrollFromTop > 350));
+  });
+
+/*Smooth scroll*/
+$('.menu li a[href^="#"]').on('click', function(e){
+  e.preventDefault();
+  var target= $(this.hash);
+  if(target.length){
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top -60 },1000);
+  }
+});
+$('.wrapper ul li a[href^="#"]').on('click', function(e){
+  e.preventDefault();
+  var target= $(this.hash);
+  if(target.length){
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top -60 },1000);
+  }
+});
+
+  /*Masonry*/
+  $('.grid').masonry({
+       //options
+       itemSelector: '.grid-item',
+       columnWidth: 120,
+       fitWidth: true,
+       gutter: 0
+   });
+
+   /*Slick slider*/
+   $('.slider').slick({
+     autoplay:true,
+     autoplaySpeed:1500,
+     centerMode:true,
+     slidesToShow:3,
+arrows:true,
+prevArrow:'<button type="button" class="slick-prev">Previous</button>',
+nextArrow:'<button type="button" class="slick-next">Previous</button>'
+
+
+   });
+
+   /*Responsive menu start*/
+   var body= $('body');
+   var menuTrigger= $('.js-menu-trigger');
+   var mainOverlay= $('.js-main-overlay');
+   menuTrigger.on('click', function(){
+     body.addClass('menu-is-active');
+   });
+
+   mainOverlay.on('click', function(){
+     body.removeClass('menu-is-active');
+   });
+   $('.menu li a').on('click', function(){
+     $('body').removeClass("menu-is-active");
+   });
+});
